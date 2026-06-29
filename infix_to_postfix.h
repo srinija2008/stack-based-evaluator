51d5cfdd67d92612fc976e0501c9130709e1b8cf
srinija2008 <srinijataduri@gmail.com>
2026-06-29 19:58:12 +0530

    Create infix_to_postfix.h

---
+++ infix_to_postfix.h
@@ -0,0 +1,11 @@
+#ifndef INFIX_TO_POSTFIX_H
+#define INFIX_TO_POSTFIX_H
+
+#include <string>
+
+using namespace std;
+
+// Converts an infix expression to postfix
+string infixToPostfix(string infix);
+
+#endif
